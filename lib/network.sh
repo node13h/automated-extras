70ab3217a80da0950f38f57bb24e1597a0704e1d
Sergej Alikov <sergej.alikov@gmail.com>
2018-07-30 19:29:05 +0100

    Support specifying protocol

--- lib/network.sh
+++ lib/network.sh
@@ -10,9 +10,10 @@ setup_port_forward () {
     local inside_address="${3}"
     local inside_port="${4}"
     local zone="${5:-public}"
+    local protocol="${6:-tcp}"
 
     msg "Setting up port forward from port ${public_port} to the ${inside_address}:${inside_port}"
 
-    cmd firewall-cmd --zone="${zone}" --add-rich-rule="rule family=ipv4 destination address=${public_address} forward-port port=${public_port} protocol=tcp to-port=${inside_port} to-addr=${inside_address}"
-    cmd firewall-cmd --zone="${zone}" --permanent --add-rich-rule="rule family=ipv4 destination address=${public_address} forward-port port=${public_port} protocol=tcp to-port=${inside_port} to-addr=${inside_address}"
+    cmd firewall-cmd --zone="${zone}" --add-rich-rule="rule family=ipv4 destination address=${public_address} forward-port port=${public_port} protocol=${protocol} to-port=${inside_port} to-addr=${inside_address}"
+    cmd firewall-cmd --zone="${zone}" --permanent --add-rich-rule="rule family=ipv4 destination address=${public_address} forward-port port=${public_port} protocol=${protocol} to-port=${inside_port} to-addr=${inside_address}"
 }
