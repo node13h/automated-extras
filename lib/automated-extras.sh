8077a5dd626e27a6b7f4ee4662ce156aa372fbc4
Sergej Alikov <sergej.alikov@gmail.com>
2018-12-25 01:57:46 +0000

    Improve wording

--- lib/automated-extras.sh
+++ lib/automated-extras.sh
@@ -8,6 +8,6 @@ supported_automated_versions 0.2
 
 supported_automated_extras_versions () {
     if ! semver_matches_one_of "${AUTOMATED_EXTRAS_VERSION}" "$@"; then
-        throw "Potentially incompatible version ${AUTOMATED_EXTRAS_VERSION} of Automated Extras detected. Supported versions are: ${*}"
+        throw "Unsupported version ${AUTOMATED_EXTRAS_VERSION} of Automated Extras detected. Supported versions are: ${*}"
     fi
 }
